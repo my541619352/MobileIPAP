c16ab9bbf045ce8fc46c30a552c2de442b15432e
my541619352 <541619352@qq.com>
2016-09-07 17:18:16 +0800

    提交测试

--- app/src/main/java/com/tjsinfo/mobileipap/MainActivity.java
+++ app/src/main/java/com/tjsinfo/mobileipap/MainActivity.java
@@ -35,9 +35,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }, new Response.ErrorListener() {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
+                    public void onErrorResponse(VolleyError error) {}
                 });
             }
         });
